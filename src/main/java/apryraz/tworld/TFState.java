71a4318b4801821c03ce001bbfb6d415b1804e31
Sergi Simon Balcells <sergisiba@gmail.com>
2020-04-20 15:26:32 +0200

    Made some javadoc

--- src/main/java/apryraz/tworld/TFState.java
+++ src/main/java/apryraz/tworld/TFState.java
@@ -21,6 +21,9 @@ public class TFState {
         initializeState();
     }
 
+    /**
+     * Initialises state
+     */
     public void initializeState() {
         for (int i = 0; i < wDim; i++) {
             for (int j = 0; j < wDim; j++) {
@@ -30,10 +33,21 @@ public class TFState {
         }
     }
 
+    /**
+     * Gets a position
+     * @param i coodinate x of position
+     * @param j coordinate y of position
+     * @return the value stored in the matrix of positions
+     */
     public String get(int i, int j) {
         return matrix[i - 1][j - 1];
     }
 
+    /**
+     * Sets a position
+     * @param pos Postion to be changed
+     * @param val value to be set. Either "?" or "X".
+     */
     public void set(Position pos, String val) {
         if ("X".equals(val))
             unknown.remove(pos);
@@ -66,6 +80,9 @@ public class TFState {
         return result;
     }
 
+    /**
+     * Prints the treasure state
+     */
     public void printState() {
         System.out.println("FINDER => Printing Treasure world matrix");
         for (int i = wDim - 1; i > -1; i--) {
@@ -77,6 +94,13 @@ public class TFState {
         }
     }
 
+    /**
+     * Loads file given the dimensions and the file as  TFState
+     * @param wDim dimension of the treasure world
+     * @param stateFile file conaining the state
+     * @return the state of this world
+     * @throws IOException if file does not exist (or other IOExceptions concerning the file)
+     */
     public static TFState loadStateFromFile(int wDim, String stateFile) throws IOException {
         TFState tfstate = new TFState(wDim);
         String row;
@@ -92,7 +116,13 @@ public class TFState {
         return tfstate;
     }
 
+    /**
+     * Hash set containing all the positions with "?" in it.
+     * It uses the idea that all updates should be done with set and
+     * the val will only be "X".
+     * @return hash set containing all unknown positions.
+     */
     public HashSet<Position> getUnknownPosition() {
-        return null;
+        return unknown;
     }
 }
